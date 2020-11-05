0912b4bac10cdab795381b374042a190d981fdf0
Jamie Egan <j.egan@mac.com>
2020-11-05 14:29:25 +0000

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require('../models/Movies');
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(allTheMoviesFromDB => {
+      res.render('movies', {movies: allTheMoviesFromDB})
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+    })
+});
+
+router.get('/movie/:movieId', (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .then(movieResult => {
+      res.render('movie', {movie: movieResult})
+    })
+    .catch(e => console.log(e))
+});
 
 module.exports = router;
